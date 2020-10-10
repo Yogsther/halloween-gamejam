6fb9cc109ebf2da81a80ca60b77d2f33fc02c7bb
Olle Kaiser <olle.kaiser@futuregames.nu>
2020-10-10 16:48:17 +0200

    Added camera script

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
+
+    int direction = 0;
+
     // Start is called before the first frame update
     void Start() {
 
     }
 
+    public void ChangeCameraLook(int way) {
+        direction += way * 90;
+    }
+
+
+
     // Update is called once per frame
     void Update() {
         float mouseX = (Input.mousePosition.x / Screen.width) / 50;
         float mouseY = (Input.mousePosition.y / Screen.height) / 50;
-        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f * direction, transform.localRotation.z));
     }
 }
