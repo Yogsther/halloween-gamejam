8025af74c0bf603eae10876ae27d232b06d0b30d
August Möller <august.moller@futuregames.nu>
2020-10-10 21:15:33 +0200

    push

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -7,6 +7,8 @@ public class Jump : Puzzle
     GameObject player;
     GameObject floor;
     GameObject obstacle;
+    bool offScreen;
+    public bool dead;
     public override void Setup()
     {
         base.Setup();
@@ -21,9 +23,9 @@ public class Jump : Puzzle
 
         player.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
         floor.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -65);
-        obstacle.GetComponent<RectTransform>().anchoredPosition = new Vector2(350, -22.5f);
+        obstacle.GetComponent<RectTransform>().anchoredPosition = new Vector2(200, -22.5f);
 
-
+        StartCoroutine(ObstacleLoop());
     }
 
     public override void Unload()
@@ -35,21 +37,81 @@ public class Jump : Puzzle
     void Update()
     {
         obstacle.GetComponent<RectTransform>().transform.Translate(-1, 0, 0);
-        if (obstacle.GetComponent<RectTransform>().anchoredPosition.x <= -350)
+        if (obstacle.GetComponent<RectTransform>().anchoredPosition.x <= -200)
         {
-            obstacle.GetComponent<RectTransform>().anchoredPosition = new Vector2(350, -22.5f);
+            offScreen = true;
+            obstacle.SetActive(false);
+            //obstacle.GetComponent<RectTransform>().anchoredPosition = new Vector2(350, -22.5f);
+        }
+        else
+        {
+            offScreen = false;
+            obstacle.SetActive(true);
         }
 
-
+        //Debug.Log(offScreen);
         if (Input.GetButtonDown("Jump") && player.GetComponent<Rigidbody2D>().velocity.y == 0)
         {
-            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 500);
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 700);
+            //player.GetComponent<Rigidbody2D>().AddForce(transform.up * 50000, ForceMode2D.Force);
             //player.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 120);
         }
+        Debug.Log(dead);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void Test()
     {
-        
-        Debug.Log("test");
+        dead = true;
+        //StartCoroutine(Fail());
     }
+    private IEnumerator ObstacleLoop()
+    {
+        yield return StartCoroutine(ObstacleOffscreen());
+        yield return StartCoroutine(ObstacleReposition());
+        
+        if (dead)
+        {
+            StartCoroutine(Fail());
+        }
+        else
+        {
+            StartCoroutine(ObstacleLoop());
+        }
+        //obstacle.GetComponent<RectTransform>().anchoredPosition = new Vector2(350, -22.5f);
+    }
+
+    IEnumerator ObstacleOffscreen()
+    {
+        int waitTime = Random.Range(0, 3);
+        //Debug.Log(waitTime);
+        while (!offScreen)
+        {
+            yield return new WaitForSeconds(waitTime);
+        }
+        
+    }
+
+    IEnumerator ObstacleReposition()
+    {
+        obstacle.GetComponent<RectTransform>().anchoredPosition = new Vector2(200, -22.5f);
+        //Failed();
+        yield return null;
+    }
+    IEnumerator Fail()
+    {
+        Debug.Log("gfdsg");
+        Failed();
+        yield return null;
+    }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == "Obstacle")
+            {
+            Test();
+            //StartCoroutine(Fail());
+            //Test();
+        }
+
+        }
+    
 }
