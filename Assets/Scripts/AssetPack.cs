dfa9cda859424ec19976e8fc1532dc57ab5de6ff
August Möller <august.moller@futuregames.nu>
2020-10-10 15:38:01 +0200

    commit

--- Assets/Scripts/AssetPack.cs
+++ Assets/Scripts/AssetPack.cs
@@ -4,4 +4,6 @@ using UnityEngine;
 
 public class AssetPack : MonoBehaviour {
     public GameObject SimonSaysButton;
+    public GameObject jumpGamePlayer;
+    public GameObject jumpGameObstacle;
 }
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : Puzzle
+{
+    GameObject player;
+    public override void Setup()
+    {
+        base.Setup();
+        Transform canvas = GameObject.Find("2DCanvas").transform;
+        GameObject player = gm.gameObject.GetComponent<AssetPack>().jumpGamePlayer;
+        player = Instantiate(player, canvas);
+        player.GetComponent<RectTransform>().anchoredPosition = new Vector2(3, 5);
+    }
+
+    public override void Unload()
+    {
+        Destroy(player);
+    }
+    void Update()
+    {
+        
+    }
+}
